ad2c359a0b7dd69eef0858198cc04826bf5666e7
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-11-26 11:41:38 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StudentManager
+{
+    class Program
+    {
+        /* Get Student Name and Grade from console input
+         * Able to handle any number of students
+         * Display all student names and grades once the user finishes adding students
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students in your class?");
+            var studentCount = int.Parse((Console.ReadLine()));
+
+            var studentNames = new string[studentCount];
+            var studentGrades = new int[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Student Name: ");
+                studentNames[i] = Console.ReadLine();
+                
+                Console.WriteLine("Student Grade: ");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }
+        }
+    }
+}
