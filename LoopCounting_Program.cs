4daf47e6fe89390324304029c901d0abc2f6fcf6
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-11-26 11:46:06 -0800

    Add files via upload

---
+++ LoopCounting_Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+ 
+ namespace LoopCounting
+ {
+     class Program
+     {
+         /* Count from 1 to 10
+          * Then back to 1
+          * Repeat five times
+          * Do not use if statements
+          */
+         static void Main(string[] args)
+         {
+             var minNum = 1;
+             var maxNum = 10;
+             var repeater = 5;
+
+             for (int j = 1; j <= repeater; j++)
+             {
+                 for (int i = minNum; i <= maxNum; i++)
+                 {
+                     Console.WriteLine(i);
+                 }
+                 for (int i = maxNum; i >= minNum; i--)
+                 {
+                     Console.WriteLine(i);
+                 }
+             }
+         }
+     }
+ }
