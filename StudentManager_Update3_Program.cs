c3a14e8e7a473eb0a1ef5a5b375436662cc865d3
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-11-26 15:13:12 -0800

    Add files via upload

---
+++ StudentManager_Update3_Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManager
+{
+    class Program
+    {
+        /* Make sure phone number property 
+         * is no longer accessible after it is set
+         */
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            var adding = true;
+            
+            while (adding)
+            {
+                var newStudent = new Student();
+                
+                Console.Write("Student Name: ");
+                newStudent.Name = Console.ReadLine();
+                
+                Console.Write("Student Grade: ");
+                newStudent.Grade = int.Parse(Console.ReadLine());
+                
+                Console.Write("Student Birthday: ");
+                newStudent.Birthday = Console.ReadLine();
+                
+                Console.Write("Student Address: ");
+                newStudent.Address = Console.ReadLine();
+                
+                Console.Write("Student Phone Number: ");
+                newStudent.SetPhone(int.Parse(Console.ReadLine()));
+                
+                students.Add(newStudent);
+                
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() != "y")
+                {
+                    adding = false;
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
+            }
+        }
+        
+        
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        private int phone;
+
+        public void SetPhone(int number)
+        {
+            phone = number;
+        }
+    }
+}
