e58cc7cdd793b897a168b297da8c7d06936b06dd
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-11-26 15:14:25 -0800

    Add files via upload

--- StudentManager_Update4_Program.cs
+++ StudentManager_Update4_Program.cs
@@ -5,8 +5,7 @@ namespace StudentManager
 {
     class Program
     {
-        /* Make sure phone number property 
-         * is no longer accessible after it is set
+        /* 
          */
         static void Main(string[] args)
         {
@@ -18,26 +17,21 @@ namespace StudentManager
             {
                 var newStudent = new Student();
                 
-                Console.Write("Student Name: ");
-                newStudent.Name = Console.ReadLine();
+                newStudent.Name = Util.Console.Ask("Student Name: ");
                 
-                Console.Write("Student Grade: ");
-                newStudent.Grade = int.Parse(Console.ReadLine());
+                newStudent.Grade = int.Parse(Util.Console.Ask("Student Grade: "));
                 
-                Console.Write("Student Birthday: ");
-                newStudent.Birthday = Console.ReadLine();
+                newStudent.Birthday = Util.Console.Ask("Student Birthday: ");
                 
-                Console.Write("Student Address: ");
-                newStudent.Address = Console.ReadLine();
+                newStudent.Address = Util.Console.Ask("Student Address: ");
                 
-                Console.Write("Student Phone Number: ");
-                newStudent.Phone = int.Parse(Console.ReadLine());
+                newStudent.Phone = int.Parse(Util.Console.Ask("Student Phone Number: "));
                 
                 students.Add(newStudent);
                 Student.Count++;
                 Console.WriteLine("Student Count: {0}", Student.Count);
                 
-                Console.WriteLine("Add another? y/n");
+                Util.Console.Ask("Add another? y/n");
 
                 if (Console.ReadLine() != "y")
                 {
